ab64c4d96ce6c3c6b63920b72ed2aadeb511c7cb
jersha <jershafelix@gmail.com>
2020-07-18 14:41:00 +0530

    bugs related to layout fixed

--- app/src/main/java/com/teamscorpion/youtubealarm/MainActivity.java
+++ app/src/main/java/com/teamscorpion/youtubealarm/MainActivity.java
@@ -1,19 +1,12 @@
 package com.teamscorpion.youtubealarm;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_SCREEN = 2000;
